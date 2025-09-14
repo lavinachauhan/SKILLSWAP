3dcc96dfb58de51cfcceea7cf7f31fc8d1af3885
lavinachauhan <chauhanlavi1627@gmail.com>
2025-09-14 15:47:43 +0530

    Signup page code added

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import SignUp from './components/SignUp'
+
+const App = () => {
+  return(
+    <div>
+      <SignUp></SignUp>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className>
+        <div className='quicksand bg-[url("./background(1).jpg")] flex flex-col justify-center items-center gap-12 h-[30rem] w-[20rem] m-auto mt-6 bg-cover'>
+            <div>
+                    <h1 className='text-[2rem] text-white font-[700]'>Create Account</h1>
+            </div>
+            <div className='flex flex-col gap-3'>
+                <input type="text" placeholder = "Full name" className='h-[2.5rem] w-[15rem] p-6 rounded-3xl bg-gray-300 focus:outline-none'/>
+                <input type="text" placeholder = "Email" className='h-[2.5rem] w-[15rem] p-6 rounded-3xl  bg-gray-300 focus:outline-none' />
+                <input type="password" placeholder = "Password" className='h-[2.5rem] w-[15rem] p-6 rounded-3xl  bg-gray-300 focus:outline-none' />
+            </div>
+            <div className=' flex flex-col gap-2.5 justify-center items-center text-gray-300'>
+                <button className='h-[3rem] w-[7rem] bg-blue-400 rounded-3xl'>Signup</button>
+                <div className='flex gap-1.5 h-[2rem] w-[10rem] justify-center items-center m-auto'> <div className='h-0.5 w-16 bg-gray-100'></div> <span>or</span> <div className='h-0.5 w-16 bg-gray-100'></div> </div>
+                <p>Already have an account ?<span className='text-blue-400 text-[1rem] font-bold'>Log in</span></p> 
+            </div>
+                          
+        </div>    
+    </div>
+  )
+}
+
+export default SignUp
